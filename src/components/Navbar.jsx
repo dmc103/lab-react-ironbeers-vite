14104b24606f73420bbdbc56e64a844b17ff4ff8
Doreen <doreenmarie_cee@hotmail.com>
2023-12-19 22:23:00 +0100

    iterations 1,2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,17 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import homeIcon from "../assets/home-icon.png";
+
+
+function Navbar() {
+    console.log("Navbar rendered");
+
+    return (
+        <nav style={{ backgroundColor: '#00BFFF', color: 'white', padding: '10px' }}>
+            <Link to="/">
+                <img src={homeIcon} alt="home icon" />
+            </Link>
+        </nav>
+    );
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,51 @@
-function HomePage() {}
+
+import { Link } from 'react-router-dom';
+import allBeers from '../assets/beers.png';
+import randomBeer from '../assets/random-beer.png';
+import newBeer from '../assets/new-beer.png';
+
+function HomePage() {
+    return (
+        <div>
+        <section>
+            <img src={allBeers} alt="all beers" />
+            <h2>All Beers</h2>
+            <Link to="/beers">All Beers</Link>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                voluptatem, voluptate, voluptatum, quia quos voluptatibus
+                officiis, quod natus consequatur quibusdam quae doloribus
+                repellendus. Quisquam, voluptatem. 
+            </p>
+        </section>
+
+        <section>
+            <img src={randomBeer} alt="random beer" />
+            <h2>Random Beer</h2>
+            <Link to="/random-beer">Random Beer</Link>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                voluptatem, voluptate, voluptatum, quia quos voluptatibus
+                officiis, quod natus consequatur quibusdam quae doloribus
+                repellendus. Quisquam, voluptatem. 
+            </p>
+        </section>
+
+        <section>
+            <img src={newBeer} alt="new beer" />
+            <h2>New Beer</h2>
+            <Link to="/new-beer">New Beer</Link>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                voluptatem, voluptate, voluptatum, quia quos voluptatibus
+                officiis, quod natus consequatur quibusdam quae doloribus
+                repellendus. Quisquam, voluptatem. 
+            </p>
+        </section>
+
+        </div>
+    );
+
+}
 
 export default HomePage;
